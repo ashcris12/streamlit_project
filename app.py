491a736d0129745a6580a33eb89b5a2726d1bbf5
ashcris12 <ashleycriswell1210@gmail.com>
2025-02-19 18:14:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -733,9 +733,8 @@ with tabs[3]:  # Feature Engineering
         sns.heatmap(corr, annot=True, cmap='seismic', ax=ax)
         st.pyplot(fig)
 
-st.write(f"DEBUG: Current role = {st.session_state.role}")
 with tabs[4]:  # Model Training
-    if st.session_state.role not in ["data_science", "finance"]:
+   if st.session_state.role not in ["data_science", "finance"]:
         st.warning("🚫 You do not have permission to access model training.")
         st.stop()
     st.title("Train a Model")
