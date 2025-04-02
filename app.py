1ba16c45519bb8a76a2b891841862ba402c48af6
ashcris12 <ashleycriswell1210@gmail.com>
2025-04-01 17:57:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -128,6 +128,7 @@ import io
 from io import BytesIO
 from fpdf import FPDF
 from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 import toml
 import getpass
 import streamlit_authenticator as stauth
